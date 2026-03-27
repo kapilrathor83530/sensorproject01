f1d170a4030089dcd59057b83607252a229d3ccc
kapilrathor <kplchhmp@gmail.com>
2026-03-27 09:50:42 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,10 @@
+import os 
+AWS_S3_BUCKET_NAME = "wafer_fault"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COULMN = "quality"
+# MONGO_DB_URL = "ye wala abhi baki hai "
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
